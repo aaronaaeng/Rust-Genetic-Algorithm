a36e0600452e6d26d9fcd93bcc96553109fe9b14
Aaron Aaeng <aaaa9270@colorado.edu>
2017-10-06 19:29:09 -0600

    Fitness

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+let gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.";
+let target = "Figure out how to adapt this.";
+
+fn generate_parent(gene_set: &String, length: usize) -> String {
+    let mut rng = thread_rng();
+    let sample = sample()
+}
