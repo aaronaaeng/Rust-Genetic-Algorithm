90b188f843745b280e5d077c164f85dec9fe6b4b
Aaron Aaeng <aaaa9270@colorado.edu>
2017-10-19 12:24:12 -0600

    Started benchmarking branch

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+//extern crate test;
 extern crate rand;
 
 use rand::{thread_rng, sample, Rng};
 
+//#[bench]
 fn main() {
     let gene_set = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,'";
     let target = "I thought it was going to Laguardia.  It's going to JFK.";
-    let best = get_best(get_fitness, display, target, gene_set, target.len());
-    println!("{}", best);
+    let result = get_best(get_fitness, display, target, gene_set, target.len());
+    println!("{} {}", result.0, result.1);
 }
 
 // Returns the target string created trhough iterations.  When a more fit string is
@@ -15,7 +17,8 @@ fn get_best(get_fitness: fn(&String,&str) -> usize,
     display: fn(&String, &str),
     target: &str,
     gene_set: &str,
-    length: usize) -> String {
+    length: usize) -> (String, i32) {
+        let mut generations = 0;
         let mut best_parent = generate_parent(gene_set, length);
         let mut best_fitness = get_fitness(&best_parent, target);
 
@@ -24,13 +27,14 @@ fn get_best(get_fitness: fn(&String,&str) -> usize,
         while best_fitness < length {
             let child = mutate_parent(&best_parent, gene_set);
             let fitness = get_fitness(&child, target);
+            generations += 1;
             if fitness > best_fitness {
                 best_fitness = fitness;
                 best_parent = child;
                 display(&best_parent, target);
             }
         }
-        best_parent
+        return (best_parent, generations);
     }
 
 // Generates a string the length of the target.  All chars are randomly selected from the
